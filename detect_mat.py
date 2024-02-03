ebec827675dcf12a520b164343446765d531668d
badim41 <ramdomakk@gmail.com>
2024-02-03 23:21:13 +1200

    update

--- detect_mat.py
+++ detect_mat.py
@@ -13,13 +13,14 @@ async def moderate_mat_in_sentence(sentence, bad_word=True):
     with open(os.path.join(BASE_DIR, "filter_profanity_russian_cached.txt"), "r", encoding="utf-8") as reader:
         lines = reader.readlines()
         mat_massive = [line.strip() for line in lines]
+        logger.logging("mat_massive:", mat_massive[:10], color=Color.GRAY)
 
     if bad_word:
         mat_massive.extend(["говн", "срал", "сраны", "срать"])
 
     sentence = re.sub(r'[^а-яА-ЯёЁa-zA-Z\s]', '', sentence)
     words = sentence.split(" ")
-    logger.logging(words, color=Color.GRAY)
+    logger.logging("words", words, color=Color.GRAY)
 
     found_mat = False
     for i, word in enumerate(words):
