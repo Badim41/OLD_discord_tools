19348719a52223f1d1b3adb61b8e4ea07f0d64af
badim41 <ramdomakk@gmail.com>
2024-02-03 19:23:55 +1200

    update

--- secret.py
+++ secret.py
@@ -1,24 +1,16 @@
 import os
 
+import asyncio
+
+from discord_tools.sql_db import set_get_database_async
+
 class SecretKey:
     voice_keys = "voice_keys"
     gpt_keys = "gpt_keys"
     gpt_auth = "gpt_auth"
 
-def create_secrets(key, value):
-    """
-    input1: keys:list, values:list
-    input2: key:str, values:list
-    """
-    if isinstance(value, list):
-        if isinstance(key, list):
-            for key_1, value_1 in zip(key, value):
-                os.environ[str(key_1)] = str(value_1)
-        else:
-            for value_1 in value:
-                os.environ[str(key)] = str(value_1)
-    else:
-        os.environ[str(key)] = str(value)
+def create_secret(key:str, value:str):
+    asyncio.run(set_get_database_async("secret", str(key), str(value)))
 
-def load_secret(key):
-    return os.environ.get(str(key))
+def load_secret(key:str):
+    return asyncio.run(set_get_database_async("secret", str(key)))
